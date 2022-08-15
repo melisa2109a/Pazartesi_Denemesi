29509f3d406bd2c6d1ca7ba3f296ab1fd1e77d82
melisa_akçay <melisaakcay69@gmail.com>
2022-08-15 11:02:32 +0300

    18.08.2022

--- src/pazartesi_ilk/C02_002.java
+++ src/pazartesi_ilk/C02_002.java
@@ -5,6 +5,7 @@ public class C02_002 {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("Hello GitHub2");
+        System.out.println("Hello GitHub2");
 
     }
 }
