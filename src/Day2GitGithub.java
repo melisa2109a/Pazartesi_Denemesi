d441a31eeb5fe526e412b410f617e24cfd722394
melisa_akçay <melisaakcay69@gmail.com>
2022-08-15 21:42:41 +0300

    mesaj

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -5,7 +5,7 @@ public class Day2GitGithub {
     ilişkilendirmek için kullanılır
     2-) git add . --> Working space'den (yani yerel) dosyalarımı staging area'ya
     (yani commitlemek için beklenen yer) gönderir
-    3-) git status --> Working space'deki ve staging area'daki durumu
+    3-) git status --> Working space'deki ve staging area'daki durumu gosterir
     4-) git commiit -m "mesaj" --> Staging area'dan commit stora
     dosyalarımı göndermek için kullanılır
     (commit yani versiyon yani sürüm oluşturmuş olurum)
@@ -15,8 +15,12 @@ public class Day2GitGithub {
             git remote add origin adress
             git push -u origin master
    NOT: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
-   kullanırız
+   kullanırız.
+   NOT: git log --oneline  --> Commitlerimdeki yani versiyonlarimdakı durumu gösterir
 
      */
+    public static void main(String[] args) {
+        System.out.println("Github'ı da öğrendim");
+    }
 
 }
